2c3d0b2250541814944bcb7d386945fb8b8df585
Martin Pabis <mpabis@sygic.com>
2023-04-07 14:18:50 +0200

    benchmark - initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// https://github.com/dotnet/BenchmarkDotNet
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run(typeof(Program).Assembly);
+
+[MemoryDiagnoser]
+public class BenchmarkTest
+{
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        //Write your initialization code here
+    }
+
+    [Benchmark]
+    public void Test1()
+    {
+        var now = DateTime.UtcNow;
+        var later = now.AddHours(1);
+    }
+}
